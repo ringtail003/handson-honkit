0ae035669452dad07fdaa12606411a89818da042
ringtail003 <ringtail003@ringtail003-macstudio.local>
2022-09-25 21:14:11 +0900

    chore: アイコン表示まで

--- docs/chapter2/base.md
+++
@@ -1,9 +0,0 @@
-{% extends "../base.html" %}
-
-{% block pageTitle %}
-<h1 class="text-lg text-red-300">太宰治</h1>
-{% endblock %}
-
-{% block contents %}
-コンテンツ
-{% endblock %}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./docs/**/*.{md,html}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      sm: ['14px', '20px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    }
   },
   plugins: [],
 }
